239fdad1a76818b9af4df68939057e52f42792b6
Tanay Gandhi <tgandhi1@sundevils.asu.edu>
2021-01-14 00:01:39 -0700

    Working version, needs testing

--- CountryData/Program.cs
+++ CountryData/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace CountryData
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            CountryInfoService.CountryInfoServiceSoapTypeClient client = new CountryInfoService.CountryInfoServiceSoapTypeClient("CountryInfoServiceSoap");
+
+            while (true)
+            {
+                Console.WriteLine("Enter country name, or type 'Q' to quit");
+                Console.WriteLine("Example format: Barbados");
+                string countryName = Console.ReadLine();
+
+                if (countryName.Equals("Q"))
+                {
+                    return;
+                }
+
+                string isoCode = client.CountryISOCode(countryName);
+                string capitalCity = client.CapitalCity(isoCode);
+                CountryInfoService.tCurrency currencyObj = client.CountryCurrency(isoCode);
+                string currencyCode = currencyObj.sISOCode;
+
+                Console.WriteLine("\nISO code:\t{0}\nCapital City:\t{1}\nCurrency Code:\t{2}\n\n", isoCode, capitalCity, currencyCode);
+            }
         }
     }
 }
